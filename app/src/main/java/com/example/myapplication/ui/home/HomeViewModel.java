7390bf675cc59a1d42d36dcbeb82310b9b759421
rezagrsn <reza.reynaldi@ti.ukdw.ac.id>
2019-11-10 15:55:42 +0700

    update Main Menu

---
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Berisi konten utama dari Gundam Hobby Support");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
