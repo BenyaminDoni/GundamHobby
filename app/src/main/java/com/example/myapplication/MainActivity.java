6fb44593e524da9555cb82edc3f487c5c1c5063e
doni <benyamin.perkasa@ti.ukdw.ac.id>
2019-11-11 18:42:10 +0700

    SignIn & SignUp (Update)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -111,4 +111,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
 }
