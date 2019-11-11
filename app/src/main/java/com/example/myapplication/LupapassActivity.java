c4bb864fdddcb5ed3b905e128f6d39b007e43385
yuangarka <apholos.yuang@ti.ukdw.ac.id>
2019-11-11 18:54:09 +0700

    revisi image view di activity

--- app/src/main/java/com/example/myapplication/LupapassActivity.java
+++ app/src/main/java/com/example/myapplication/LupapassActivity.java
@@ -3,12 +3,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class LupapassActivity extends AppCompatActivity {
+    ImageView v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lupapass);
+
+        ImageView v = (ImageView) findViewById(R.id.imageView);
+        v.setImageResource(R.drawable.gundam_projek_andro_logo);
     }
 }
