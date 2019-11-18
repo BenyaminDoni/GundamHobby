bdab79864c1947e72584df6fc11711a1d5090a89
yuangarka <apholos.yuang@ti.ukdw.ac.id>
2019-11-18 15:25:35 +0700

    Tambah fragment list-gundam dan wishlist di home

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -29,5 +32,37 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intToMain);
             }
         });
+    }
+
+    public void loadFragmentOne(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment1 = fragmentManager.findFragmentByTag("fragOne");
+
+        if (fragment1 == null) {
+            fragment1 = new ListFragment();
+        }
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.mainFrag, fragment1, "fragOne");
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void loadFragmentTwo(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment2 = fragmentManager.findFragmentByTag("fragTwo");
+
+        if (fragment2 == null) {
+            fragment2 = new WishFragment();
+        }
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.mainFrag, fragment2, "fragTwo");
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/myapplication/WishFragment.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class WishFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_wish,container,false);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
