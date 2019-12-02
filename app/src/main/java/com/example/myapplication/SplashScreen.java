065201f252fec576992db790ae6d30ce50b7ceb3
doni <benyamin.perkasa@ti.ukdw.ac.id>
2019-12-02 18:50:12 +0700

    Splash Screen

---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private int waktu_loading=4000;
+
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+
+}
